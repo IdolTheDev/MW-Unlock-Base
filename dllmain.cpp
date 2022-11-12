884fb8796c7c010a377079c87699d1dc82dc9575
KCtheDev <78183152+KCtheDev@users.noreply.github.com>
2022-11-11 19:22:54 -0800

    Update dllmain.cpp

--- dllmain.cpp
+++ dllmain.cpp
@@ -1,5 +1,12 @@
 #include <Windows.h>
 
+void memcopy(BYTE* dst, BYTE* src, size_t size) {
+    DWORD oldprotect;
+    VirtualProtect(dst, size, PAGE_EXECUTE_READWRITE, &oldprotect);
+    memcpy(dst, src, size);
+    VirtualProtect(dst, size, oldprotect, &oldprotect);
+}
+
 namespace mem
 {
 	template<typename T> T read(uintptr_t address) {
@@ -11,6 +18,22 @@ namespace mem
 		try { *(T*)address = value; }
 		catch (...) { return; }
 	}
+}
+
+uintptr_t base = (uintptr_t)GetModuleHandle(NULL);
+uintptr_t unlockoffset = 0x0;
+void unlock()
+{
+    uintptr_t num = (base + unlockoffset + 0xC);
+    int num2 = mem::read<int>(num);
+    uintptr_t unlock_base = num + num2 + 4 - base;
+    uintptr_t numP = (base + unlock_base + 0x60);
+    DWORD oldprotect;
+    memcopy((BYTE*)numP, (BYTE*)"\x48\x83\xC4\x08\x48\x8B\x5C\x24\x30\x48\x8B\x74\x24\x38\x48\x83\xC4\x20\x5F\x48\xC7\xC0\x01\x00\x00\x00\xC3", 28);
+    VirtualProtectEx(GetCurrentProcess(), (BYTE*)base + unlock_base, sizeof(base + unlock_base), PAGE_READWRITE, &oldprotect);
+    mem::write<uintptr_t>(base + unlock_base + 8, numP);
+    mem::write<uintptr_t>(base + unlock_base, base + unlock_base);
+    VirtualProtect((BYTE*)base + unlock_base, sizeof(base + unlock_base), oldprotect, &oldprotect);
 }
 
 void main()
